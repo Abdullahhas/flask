93da585d65ff768909652b36cdca21f097e09774
abdullahhas <abdullah312003@gmail.com>
2025-07-27 19:34:22 +0500

    sqllite viewer and add records

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask , render_template
+from flask import Flask, render_template
 from flask_sqlalchemy import SQLAlchemy
 from datetime import datetime
 
@@ -6,23 +6,29 @@ app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = "sqlite:///todo.db"
 db = SQLAlchemy(app)
 
-# Defining schema 
+# ✅ Defining schema 
 class Todo(db.Model):
-    sno = db.Column(db.Integer , primary_key=True)
-    title = db.Column(db.String(200) , nullable=False)
-    desc = db.Column(db.String(500) , nullable=False)
-    date_created = db.Column(db.DateTime , default = datetime.utcnow)
+    sno = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String(200), nullable=False)
+    desc = db.Column(db.String(500), nullable=False)
+    date_created = db.Column(db.DateTime, default=datetime.utcnow)
 
     def __repr__(self) -> str:
         return f"{self.sno} - {self.title}"
 
 @app.route("/")
 def hello_world():
+    todo = Todo(title = "First tod" , desc = "Start investing in the Stock market")
+    db.session.add(todo)
+    db.session.commit()
     return render_template('index.html')
 
 @app.route("/products")
 def product():
     return 'this is a product page'
- 
+
 if __name__ == "__main__":
+    with app.app_context():
+        print("✅ Running db.create_all()")
+        db.create_all()
     app.run(debug=True)
